aosp-xi-sdm845/device_xiaomi_dipper

dipper: Make shim for com.vidhance.node.eis
P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/vendor/lib/camera/component/com.vidhance.node.eis.so"

Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

extract-files.sh
@@ -20,4 +20,8 @@ export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
 
+# Load com.vidhance.node.eis shim
+VIDHANCE_EIS="$BLOB_ROOT"/vendor/lib/camera/component/com.vidhance.node.eis.so
+patchelf --add-needed com.vidhance.node.eis.shim.so "$VIDHANCE_EIS"
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

noobrepos-beryllium/android_device_xiaomi_beryllium

Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

extract-files.sh
@@ -35,4 +35,8 @@ export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2018
 
+# Load com.vidhance.node.eis shim
+VIDHANCE_EIS="$BLOB_ROOT"/vendor/lib/camera/components/com.vidhance.node.eis.so
+patchelf --add-needed com.vidhance.node.eis.shim.so "$VIDHANCE_EIS"
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"