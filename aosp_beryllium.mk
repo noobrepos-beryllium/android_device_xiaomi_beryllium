021f44c53226fde271be103d53c59bbc1bea1736
shivatejapeddi <shivatejapeddi@gmail.com>
2020-04-20 15:26:46 +0800

    beryllium: AEX Bringup

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,14 +6,26 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot Animation res
+#TARGET_DENSITY := xxxhdpi
+TARGET_BOOT_ANIMATION_RES := 2280
+
+# Build with GApps if GAPPS_BUILD is true
+#ifeq ($(GAPPS_BUILD),true)
+#    WITH_GAPPS := true
+#    TARGET_GAPPS_ARCH := arm64
+#    IS_PHONE := true
+#    TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+#endif
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
