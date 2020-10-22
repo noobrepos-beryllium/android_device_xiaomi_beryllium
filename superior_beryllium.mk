67f4dad114aeb829a2b543629b70d9cd2bd77781
RucioTozzy <fallgray@gmail.com>
2020-10-23 07:51:40 +0800

    SuperiorOS Bringup

--- superior_beryllium.mk
+++ superior_beryllium.mk
@@ -6,11 +6,15 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Boot Animation res
+TARGET_DENSITY := xxxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := superior_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
