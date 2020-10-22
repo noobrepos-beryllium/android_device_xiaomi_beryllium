0706008911534fa3fe9887b1b297d65725553a0e
RucioTozzy <fallgray@gmail.com>
2020-10-23 07:51:40 +0800

    RevengeOS Bringup

--- revengeos_beryllium.mk
+++ revengeos_beryllium.mk
@@ -6,11 +6,18 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Boot Animation res
+TARGET_DENSITY := xxxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Some Extra's
+REVENGEOS_BUILDTYPE := UNOFFICIAL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := revengeos_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
